bb6cc876a631436a3c0af5f681506919cb77a8b9
AlrxDaley <alexander_daley@icloud.com>
2022-02-21 19:03:09 +0000

    blog project attempt 2 innit

---
+++ blog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Post, Comments
+from django_summernote.admin import SummernoteModelAdmin
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    prepopulated_fields = {'slug':('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title','slug','status','created_on')
+    search_fields = ['title','contents']
+    summernote_field = ('contents')
+
+
+@admin.register(Comments)
+class CommentAdmin(SummernoteModelAdmin):
+    list_display = ('name','body','post','created_on')
+    list_filer = ('approved','created_on')
+    search_fields = ['name','email_address','body']
+    
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    
+    
+
+
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = 'postgres://uaazwgjaipytiq:7fe71449917a9ab43b4a21ddbc7059002a42efb54e3e9cfff52724597f07c8df@ec2-176-34-168-83.eu-west-1.compute.amazonaws.com:5432/d5a1j3f5it3i3l'
+os.environ["SECRET_KEY"] = 'Secret_key2000'
+os.environ["CLOUDINARY_URL"] = 'cloudinary://859435968898514:wKr-IzomNbfUz1BpGPSlu-Yhnk8@dkpchnxiu'
